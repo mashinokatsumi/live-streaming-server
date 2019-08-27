e8fe17b9df264e74db134555563a8214aa29339b
SweetMei25 <41826639+SweetMei25@users.noreply.github.com>
2019-08-27 16:54:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  
-  console.log('a user connected: ' + socket.id);
+io.on('connection', function (socket) {
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    console.log('a user connected: ' + socket.id);
+
+    var serverID = 'undefined';
+
+    socket.on('RegServerId', function (data) {
+        serverID = socket.id;
+    });
+
+      socket.on('disconnect', function(){
+          if (serverID == socket.id) {
+              console.log('removed Server: ' + socket.id);
+              serverID = 'undefined';
+          }
+          else {
+              console.log('user disconnected: ' + socket.id);
+          }
+      });
 
     socket.on('webimage-byte', function(data){
-        //console.log('received: ' + data.ByteData.length);
-    	socket.emit('webimage-byte', {
+    	io.emit('webimage-byte', {
     	    Width:data.Width,
     	    Height:data.Height,
             ByteData:data.ByteData,
     	    Base64Str:data.Base64Str
-            });
-    	socket.broadcast.emit('webimage-byte', {
-    	    Width:data.Width,
-    	    Height:data.Height,
-            ByteData:data.ByteData,
-    	    Base64Str:data.Base64Str
-            });
-	
+        });
     });
 
     socket.on('DemoReceive', function(data){
@@ -36,13 +40,41 @@ io.on('connection', function(socket){
         console.log('received: ' + data.name);
 
         socket.emit('DemoReceive', {
-        name:data.name
+            name:data.name
         });
         socket.broadcast.emit('DemoReceive', {
-        name:data.name
+            name:data.name
         });
-    
     });
+
+    socket.on('OnReceiveData', function (data) {
+        if (serverID != 'undefined') {
+            if (data.EmitType == 0) {
+                io.emit('OnReceiveData', {
+                    DataString: data.DataString,
+                    DataByte: data.DataByte
+                });
+            }
+
+            if (data.EmitType == 1) {
+                io.to(serverID).emit('OnReceiveData', {
+                    DataString: data.DataString,
+                    DataByte: data.DataByte
+                });
+            }
+
+            if (data.EmitType == 2) {
+                socket.broadcast.emit('OnReceiveData', {
+                    DataString: data.DataString,
+                    DataByte: data.DataByte
+                });
+            }
+        }
+        else {
+            console.log('cannot find any active server');
+        }
+    });
+
 });
 
 http.listen(3000, function(){
